e9900779e661eb01af7463b1f5efce2a388bba76
SuperValou <valentin.esmieu@tuta.io>
2020-05-21 21:30:39 +0200

    Add weapons from WGJ144

---
+++ Integration/Assets/Scripts/Weapons/AbstractWeapon.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Weapons
+{
+    public abstract class AbstractWeapon : MonoBehaviour
+    {
+        /// <summary>
+        /// What to do when the trigger is pressed and held
+        /// </summary>
+        public abstract void Fire();
+
+        /// <summary>
+        /// What to do when the trigger is released
+        /// </summary>
+        public abstract void ReleaseFire();
+    }
+}
