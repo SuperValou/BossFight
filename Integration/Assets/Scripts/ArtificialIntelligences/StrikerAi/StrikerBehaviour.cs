f65628f17aeb8d38d7151936128cc9090fed8e5d
SuperValou <valentin.esmieu@tuta.io>
2020-06-20 21:02:30 +0200

    Lay ground for boss AI and animations

---
+++ Integration/Assets/Scripts/ArtificialIntelligences/StrikerAi/StrikerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.ArtificialIntelligences.StrikerAi
+{
+    public abstract class StrikerBehaviour : StateMachineBehaviour, IBehaviour<StrikerStateMachine>
+    {
+        protected StrikerStateMachine StateMachine;
+        
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            StateMachine.SetCurrentBehaviour(this);
+        }
+
+        public void Initialize(StrikerStateMachine stateMachine)
+        {
+            StateMachine = stateMachine ?? throw new ArgumentNullException(nameof(stateMachine));
+        }
+    }
+}
---
+++ Integration/Assets/Scripts/ArtificialIntelligences/StrikerAi/StrikerStateMachine.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Utilities;
+using UnityEngine;
+
+namespace Assets.Scripts.ArtificialIntelligences.StrikerAi
+{
+    public class StrikerStateMachine : MonoBehaviour, IStateMachine<StrikerBehaviour>
+    {
+        private Animator _animator;
+        private StrikerBehaviour[] _behaviours;
+        
+        void Start()
+        {
+            _animator = this.GetOrThrow<Animator>();
+            _behaviours = _animator.GetBehaviours<StrikerBehaviour>();
+
+            foreach (var strikerBehaviour in _behaviours)
+            {
+                strikerBehaviour.Initialize(this);
+            }
+        }
+        
+        public void SetCurrentBehaviour(StrikerBehaviour behaviour)
+        {
+            Debug.Log($"Current behaviour: {behaviour.GetType().Name}");
+        }
+    }
+}
