af82fc88dd3b716f606dec4b1ce62f049ea20ee9
SuperValou <valentin.esmieu@tuta.io>
2020-07-20 23:34:01 +0200

    Create most basic boss battle room

--- Integration/Assets/Scripts/Foes/Striker.cs
+++ Integration/Assets/Scripts/Foes/Striker.cs
@@ -1,13 +1,22 @@
-﻿using Assets.Scripts.Utilities;
+﻿using Assets.Scripts.Damages;
+using Assets.Scripts.Huds;
+using Assets.Scripts.Utilities;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace Assets.Scripts.ArtificialIntelligences.StrikerAi
 {
-    public class Striker : MonoBehaviour, IStateMachine<StrikerBehaviour>
+    public class Striker : Damageable, IStateMachine<StrikerBehaviour>
     {
+        // -- Editor
         [Header("References")]
+        [Tooltip("Target of the " + nameof(Striker) + ". Can be null.")]
         public Transform target;
+
+        [Tooltip("System displaying the "+ nameof(Striker) + "'s health on screen. Can be null.")]
+        public FoeHealthDisplayProxy foeHealthDisplayProxy;
+
+        // -- Class
 
         private Animator _animator;
         private StrikerBehaviour[] _behaviours;
@@ -45,6 +54,20 @@ namespace Assets.Scripts.ArtificialIntelligences.StrikerAi
         public void SetCurrentBehaviour(StrikerBehaviour behaviour)
         {
             Debug.Log($"Current behaviour: {behaviour.GetType().Name}");
+        }
+
+        protected override void OnDamageTaken()
+        {
+            if (foeHealthDisplayProxy != null)
+            {
+                foeHealthDisplayProxy.Show((Damageable) this);
+            }
+        }
+
+        protected override void Die()
+        {
+            // Victory
+            Debug.LogWarning("Victory!");
         }
     }
 }
