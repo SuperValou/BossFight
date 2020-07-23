78bbd49d67f736853571f1a850ef35196678efee
SuperValou <valentin.esmieu@tuta.io>
2020-07-23 21:50:27 +0200

    Adapt scenes to the new loading system

--- Integration/Assets/Bank/LoadingSystems/SceneInfos/SceneId.cs
+++ Integration/Assets/Bank/LoadingSystems/SceneInfos/SceneId.cs
@@ -8,22 +8,7 @@
         [SceneInfo("0-Gameplay", SceneType.Gameplay)]
         GameplayScene,
 
-        [SceneInfo("1-FirstRoom", SceneType.Room)]
-        WakeUpRoomScene,
-
-        [SceneInfo("2-TransitionCorridor", SceneType.Room)]
-        CorridorRoomScene,
-
-        [SceneInfo("3-BossRoom", SceneType.Room)]
+        [SceneInfo("1-BossRoom", SceneType.Room)]
         BossRoomScene,
-
-        [SceneInfo("3-1-Corridor-A", SceneType.Room)]
-        CorridorA,
-
-        [SceneInfo("3-2-Corridor-B", SceneType.Room)]
-        CorridorB,
-
-        [SceneInfo("3-3-Corridor-C", SceneType.Room)]
-        CorridorC
     }
 }
