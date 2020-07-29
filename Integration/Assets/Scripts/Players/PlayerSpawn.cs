d3ac00cfcdf8df2af295f040c244f5a9868479da
SuperValou <valentin.esmieu@tuta.io>
2020-07-29 20:47:56 +0200

    Add rooms

---
+++ Integration/Assets/Scripts/Players/PlayerSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Players
+{
+    public class PlayerSpawn : MonoBehaviour
+    {
+        private Player _player;
+
+        void Start()
+        {
+            _player = GameObject.FindObjectOfType<Player>();
+            if (_player == null)
+            {
+                Debug.LogWarning($"{nameof(Player)} was not found in hierarchy. It won't be spawned at '{this.transform.position}'.");
+                return;
+            }
+
+            _player.transform.position = this.transform.position;
+            _player.transform.rotation = this.transform.rotation;
+        }
+    }
+}
