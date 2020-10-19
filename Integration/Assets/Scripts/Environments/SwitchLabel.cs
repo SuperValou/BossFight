75292612c4c45e9181d1d97f93ea49eb6fb32d7a
SuperValou <valentin.esmieu@tuta.io>
2020-10-19 20:33:00 +0200

    Move turret activation switch, add walls to room

---
+++ Integration/Assets/Scripts/Environments/SwitchLabel.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Utilities;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Environments
+{
+    [RequireComponent(typeof(TextMeshPro))]
+    public class SwitchLabel : MonoBehaviour
+    {
+        // -- Editor
+
+        public ActivationSwitch activationSwitch;
+        public string enabledText;
+        public string disabledText;
+
+        // -- Class
+
+        private TextMeshPro _label;
+        
+        void Start()
+        {
+            _label = this.GetOrThrow<TextMeshPro>();
+        }
+
+        void Update()
+        {
+            _label.SetText(activationSwitch.IsTurnedOn ? enabledText : disabledText);
+        }
+    }
+}
