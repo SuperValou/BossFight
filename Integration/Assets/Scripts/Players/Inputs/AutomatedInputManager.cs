1dfd50114de70301464dbaecc91c229c83ba0440
SuperValou <valentin.esmieu@tuta.io>
2020-10-19 20:34:19 +0200

    Class stub

---
+++ Integration/Assets/Scripts/Players/Inputs/AutomatedInputManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Players.Inputs
+{
+    public class AutomatedInputManager : AbstractInputManager
+    {
+        public override Vector2 GetLookVector()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Vector3 GetMoveVector()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool FireButtonDown()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool FireButton()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool FireButtonUp()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool JumpButton()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool JumpButtonDown()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool BoosterButtonDown()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool DashButtonDown()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool SwitchWeaponDown(out WeaponSwitchDirection weaponSwitchDirection)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
