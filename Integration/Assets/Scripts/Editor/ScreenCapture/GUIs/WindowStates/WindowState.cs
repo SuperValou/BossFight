4f3ea8544f5ebd86f4790a02094bdd47d611d067
SuperValou <valentin.esmieu@tuta.io>
2020-11-07 22:53:23 +0100

    Delete unusable editor tool

--- Integration/Assets/Scripts/Editor/ScreenCapture/GUIs/WindowStates/WindowState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assets.Scripts.Editor.ScreenCapture.GUIs.WindowStates
-{
-    public interface IWindowState
-    {
-        WindowStateName Name { get; }
-
-        IWindowState OnGui();
-    }
-}
--- Integration/Assets/Scripts/Editor/ScreenCapture/GUIs/WindowStates/WindowStateName.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assets.Scripts.Editor.ScreenCapture.GUIs.WindowStates
-{
-    public enum WindowStateName
-    {
-        SettingUp,
-        Recording,
-        Encoding
-    }
-}
