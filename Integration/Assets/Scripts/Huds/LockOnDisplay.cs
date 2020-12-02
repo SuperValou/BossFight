5bcc79ac0b7b9ae20537c8dca277746fe1727410
SuperValou <valentin.esmieu@tuta.io>
2020-12-02 01:10:04 +0100

    Add lock-on animations

--- Integration/Assets/Scripts/Huds/LockOnDisplay.cs
+++ Integration/Assets/Scripts/Huds/LockOnDisplay.cs
@@ -13,11 +13,20 @@ namespace Assets.Scripts.Huds
         [Tooltip("Angular speed of the lock circle (degree per second).")]
         public float angularSpeed = 2f;
 
-        [Tooltip("Time to fade in on lock-on (seconds).")]
-        public float lockOnFadeIn = 0.1f;
+        [Tooltip("Time to fade in/fade out when lock-in/locking-out (second).")]
+        public float circleTweenTime = 0.25f;
 
-        [Tooltip("Time to fade out on lock break (seconds).")]
-        public float lockBreakFadeOut = 0.1f;
+        [Tooltip("Multiplier applied on the object scale for the transition (scalar).")]
+        public float circleScaleMultiplier = 1.5f;
+
+        [Tooltip("Time to fade out on lock break (second).")]
+        public float breakCircleTweenTime = 1f;
+
+        [Tooltip("Strength of the shaking effect on lock break (unknown unit).")]
+        public float breakCircleShakeStrength = 100f;
+
+        [Tooltip("Number of time per second the shake effect will occur on lock break (hertz).")]
+        public int breakCircleShakeFrequency = 30;
 
         [Header("Parts")]
         public Image lockCircle;
@@ -27,13 +36,27 @@ namespace Assets.Scripts.Huds
 
         // -- Class
 
-        private Color lockCircleInitialColor;
-        private Color fadedOutLockCircleColor;
+        private Vector3 _lockCircleInitialPosition;
+        private Vector3 _lockCircleInitialScale;
+        private Color _lockCircleInitialColor;
+
+        private Color _fadedOutLockCircleColor;
+        private Vector3 _fadedOutLockCircleScale;
+
+        
+
+
+        private Tween _runningTween;
 
         void Start()
         {
-            lockCircleInitialColor = lockCircle.color;
-            fadedOutLockCircleColor = new Color(lockCircleInitialColor.r, lockCircleInitialColor.g, lockCircleInitialColor.b, a: 0);
+            _lockCircleInitialPosition = lockCircle.transform.position;
+            _lockCircleInitialScale = lockCircle.transform.localScale;
+            _lockCircleInitialColor = lockCircle.color;
+            
+            _fadedOutLockCircleColor = new Color(_lockCircleInitialColor.r, _lockCircleInitialColor.g, _lockCircleInitialColor.b, a: 0);
+            _fadedOutLockCircleScale = lockCircle.transform.localScale * circleScaleMultiplier;
+
             lockCircle.gameObject.SetActive(false);
         }
 
@@ -47,20 +70,65 @@ namespace Assets.Scripts.Huds
 
         public void OnLockOn()
         {
-            lockCircle.color = fadedOutLockCircleColor;
-            lockCircle.gameObject.SetActive(true);
-            lockCircle.DOColor(lockCircleInitialColor, lockOnFadeIn);
-        }
+            _runningTween?.Kill();
 
-        public void OnLockBreak()
-        {
-            var tween = lockCircle.DOColor(fadedOutLockCircleColor, lockBreakFadeOut);
-            tween.OnComplete(() => lockCircle.gameObject.SetActive(false));
+            lockCircle.transform.position = _lockCircleInitialPosition;
+            lockCircle.transform.localScale = _fadedOutLockCircleScale;
+            lockCircle.color = _fadedOutLockCircleColor;
+            
+            lockCircle.gameObject.SetActive(true);
+            
+            var colorTween = lockCircle.DOColor(_lockCircleInitialColor, circleTweenTime);
+            var scaleTween = lockCircle.transform.DOScale(_lockCircleInitialScale, circleTweenTime);
+
+            Sequence tweenSequence = DOTween.Sequence();
+            tweenSequence.Insert(0, colorTween);
+            tweenSequence.Insert(0, scaleTween);
+            tweenSequence.OnComplete(() => _runningTween = null);
+
+            _runningTween = tweenSequence;
         }
 
         public void OnUnlock()
         {
-            lockCircle.gameObject.SetActive(false);
+            _runningTween?.Kill();
+
+            var colorTween = lockCircle.DOColor(_fadedOutLockCircleColor, circleTweenTime);
+            var scaleTween = lockCircle.transform.DOScale(_fadedOutLockCircleScale, circleTweenTime);
+
+            Sequence tweenSequence = DOTween.Sequence();
+            tweenSequence.Insert(0, colorTween);
+            tweenSequence.Insert(0, scaleTween);
+
+            tweenSequence.OnComplete(() =>
+            {
+                lockCircle.gameObject.SetActive(false);
+                _runningTween = null;
+            });
+
+            _runningTween = tweenSequence;
         }
+
+        public void OnLockBreak()
+        {
+            _runningTween?.Kill();
+
+            var colorTween = lockCircle.DOColor(_fadedOutLockCircleColor, breakCircleTweenTime);
+            var shakeTween = lockCircle.transform.DOShakePosition(breakCircleTweenTime,  strength: breakCircleShakeStrength, vibrato: breakCircleShakeFrequency);
+
+            Sequence tweenSequence = DOTween.Sequence();
+            tweenSequence.Insert(0, colorTween);
+            tweenSequence.Insert(0, shakeTween);
+
+            tweenSequence.OnComplete(() =>
+            {
+                lockCircle.gameObject.SetActive(false);
+                _runningTween = null;
+            });
+
+            _runningTween = tweenSequence;
+        }
+
+        
     }
 }
