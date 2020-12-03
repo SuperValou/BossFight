49b1a47f53b9e377a4fbc93be8b6ed0df8613644
SuperValou <valentin.esmieu@tuta.io>
2020-12-03 23:28:31 +0100

    Move transition classes into another folder

--- Integration/Assets/Scripts/Huds/LockOnDisplay.cs
+++ Integration/Assets/Scripts/Huds/LockOnDisplay.cs
@@ -1,5 +1,5 @@
-﻿using Assets.Scripts.Players.LockOns;
-using DG.Tweening;
+﻿using Assets.Scripts.Huds.Transitions;
+using Assets.Scripts.Players.LockOns;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -99,119 +99,6 @@ namespace Assets.Scripts.Huds
         public void OnLockableOutOfSight()
         {
             _hintCircleTransition.FadeOut();
-        }
-    }
-
-    internal class FadeTransition
-    {
-        private readonly float _fadeDuration;
-        private readonly float _scaleMultiplier;
-
-        private Vector3 _initialPosition;
-        private Vector3 _initialScale;
-        private Color _initialColor;
-
-        protected Image Image { get; }
-
-        protected Color OutColor { get; private set; }
-        protected Vector3 OutScale { get; private set; }
-
-        protected Tween RunningTween { get; set; }
-
-        public FadeTransition(Image image, float scaleMultiplier, float fadeDuration)
-        {
-            Image = image;
-
-            _scaleMultiplier = scaleMultiplier;
-            _fadeDuration = fadeDuration;
-        }
-
-        public void Initialize()
-        {
-            _initialPosition = Image.transform.position;
-            _initialScale = Image.transform.localScale;
-            _initialColor = Image.color;
-
-            OutColor = new Color(_initialColor.r, _initialColor.g, _initialColor.b, a: 0);
-            OutScale = Image.transform.localScale * _scaleMultiplier;
-
-            Image.gameObject.SetActive(false);
-        }
-
-        public void FadeIn()
-        {
-            RunningTween?.Kill();
-
-            Image.transform.position = _initialPosition;
-            Image.transform.localScale = OutScale;
-            Image.color = OutColor;
-
-            Image.gameObject.SetActive(true);
-
-            var colorTween = Image.DOColor(_initialColor, _fadeDuration);
-            var scaleTween = Image.transform.DOScale(_initialScale, _fadeDuration);
-
-            Sequence tweenSequence = DOTween.Sequence();
-            tweenSequence.Insert(0, colorTween);
-            tweenSequence.Insert(0, scaleTween);
-            tweenSequence.OnComplete(() => RunningTween = null);
-
-            RunningTween = tweenSequence;
-        }
-
-        public void FadeOut()
-        {
-            RunningTween?.Kill();
-
-            var colorTween = Image.DOColor(OutColor, _fadeDuration);
-            var scaleTween = Image.transform.DOScale(OutScale, _fadeDuration);
-
-            Sequence tweenSequence = DOTween.Sequence();
-            tweenSequence.Insert(0, colorTween);
-            tweenSequence.Insert(0, scaleTween);
-
-            tweenSequence.OnComplete(() =>
-            {
-                Image.gameObject.SetActive(false);
-                RunningTween = null;
-            });
-
-            RunningTween = tweenSequence;
-        }
-    }
-
-    internal class ShakeOutFadeTransition : FadeTransition
-    {
-        private readonly float _shakeTime;
-        private readonly float _shakeStrength;
-        private readonly int _shakeFrequency;
-
-        public ShakeOutFadeTransition(Image image, float scaleMultiplier, float fadeDuration, float shakeTime, float shakeStrength, int shakeFrequency) 
-            : base(image, scaleMultiplier, fadeDuration)
-        {
-            _shakeTime = shakeTime;
-            _shakeStrength = shakeStrength;
-            _shakeFrequency = shakeFrequency;
-        }
-
-        public void ShakeOut()
-        {
-            RunningTween?.Kill();
-
-            var colorTween = Image.DOColor(OutColor, _shakeTime);
-            var shakeTween = Image.transform.DOShakePosition(_shakeTime, strength: _shakeStrength, vibrato: _shakeFrequency);
-
-            Sequence tweenSequence = DOTween.Sequence();
-            tweenSequence.Insert(0, colorTween);
-            tweenSequence.Insert(0, shakeTween);
-
-            tweenSequence.OnComplete(() =>
-            {
-                Image.gameObject.SetActive(false);
-                RunningTween = null;
-            });
-
-            RunningTween = tweenSequence;
         }
     }
 }
